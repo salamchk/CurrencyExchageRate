b234c2589d42f6e0a4a360b7d5a9282dc54b5992
Oleksandr Solomovych <oleksandr.solomovych@edvantis.com>
2021-05-26 16:39:59 +0300

    rename method

--- CurrencyExchageRate/wwwroot/js/site.js
+++ CurrencyExchageRate/wwwroot/js/site.js
@@ -8,10 +8,11 @@ GetRates();
 function GetRates() {
     select.innerHTML = ''; fetch(DateToString(date.value), {
         method: 'POST',
-        headers: {'Content-Type': 'application/json;charset=utf-8'}}).then(response => response.json()).then(data => DataFromResponseToSelect(data));
+        headers: { 'Content-Type': 'application/json;charset=utf-8' }
+    }).then(response => response.json()).then(data => DataToSelect(data));
 }
 
-function DataFromResponseToSelect(data) {data.forEach(item => {
+function DataToSelect(data) {data.forEach(item => {
         var currency = document.createElement('option');
         currency.value = item.rate;
         currency.innerHTML = item.fullName;
