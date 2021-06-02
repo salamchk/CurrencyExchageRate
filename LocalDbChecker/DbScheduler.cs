912785b8b965bc8faf9c34b8930becc8889c93fc
Oleksandr Solomovych <oleksandr.solomovych@edvantis.com>
2021-06-02 17:08:54 +0300

    threads

--- LocalDbChecker/DbScheduler.cs
+++ LocalDbChecker/DbScheduler.cs
@@ -1,10 +1,7 @@
 ﻿using Quartz;
 using Quartz.Impl;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace WebApplication2
 {
@@ -14,23 +11,19 @@ namespace WebApplication2
         {
             IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
             await scheduler.Start();
-            int triggerId = 0;
-            for (DateTime date = DateTime.Today.AddDays(-30); date < DateTime.Today; date = date.AddDays(1))
-            {
-                //Create job and add some context data
-                IJobDetail job = JobBuilder.Create<DbParser>()
-                    .UsingJobData("connnectionString", connectionString)
-                    .UsingJobData("uri", uri)
-                    .UsingJobData("date",date.ToShortDateString()).Build();
-                
-                ITrigger trigger = TriggerBuilder.Create()
-                    .WithIdentity($"trigger{triggerId++}", "group1")
-                    .StartNow().WithDailyTimeIntervalSchedule(x=>x
-                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0,0)))
-                    .Build();
 
-                await scheduler.ScheduleJob(job, trigger);
-            }
+            //Create job and add some context data
+            IJobDetail job = JobBuilder.Create<GetCurrencyRatesJob>()
+               .UsingJobData("connnectionString", connectionString)
+               .UsingJobData("uri", uri)
+               .UsingJobData("date", DateTime.Today.ToShortDateString()).Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+          .WithIdentity("trigger", "group1")
+          .StartNow().WithDailyTimeIntervalSchedule(x => x
+          .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 0)))
+          .Build();
+            await scheduler.ScheduleJob(job, trigger);
         }
     }
 }
--- LocalDbChecker/GetCurrencyRatesJob.cs
+++ LocalDbChecker/GetCurrencyRatesJob.cs
@@ -2,29 +2,47 @@
 using CurrencyRateLibrary.WebClientBank;
 using Quartz;
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace WebApplication2
 {
     public class GetCurrencyRatesJob : IJob
     {
+        private static string connectionString;
+        private static string uri;
+
         public async Task Execute(IJobExecutionContext context)
         {
             Console.WriteLine("In DBParser");
             var dataMap = context.JobDetail.JobDataMap;
             //Get data from context
-            var connectionString = dataMap.GetString("connnectionString");
-            var uri = dataMap.GetString("uri");
+            connectionString = dataMap.GetString("connnectionString");
+            uri = dataMap.GetString("uri");
             var date = dataMap.GetDateTime("date");
             //Create providers for updating database
-            GetRates(connectionString, uri, date);
+            GetRates(date);
         }
 
-        private static void GetRates(string connectionString, string uri, DateTime date)
+        private static void GetRates(DateTime date)
+        {
+            var semaphore = new Semaphore(30, 30);
+            for (DateTime currentDate = date.AddDays(-30); currentDate < date; currentDate = currentDate.AddDays(1))
+            {
+                semaphore.WaitOne();
+                Thread thread = new Thread(new ParameterizedThreadStart(Save));
+                thread.Start(currentDate);
+                semaphore.Release();
+            }
+        }
+
+
+        private static void Save(object currentDate)
         {
             var dbProvider = new DbDataProvider(connectionString);
             var apiProvider = new WebApiData(uri);
-
+            var date = (DateTime)currentDate;
             var rates = dbProvider.GetCurrencyExchangeRate(date);
             if (rates != null && rates.Count > 0)
             {
